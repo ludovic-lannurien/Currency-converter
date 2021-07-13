10a632c4802034c96f0fcc6cc69f77d60189c94c
ludovic-lannurien <ludovic.lannurien@gmail.com>
2021-07-13 12:02:23 +0200

    decouverte des conditions en react et jsx

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,18 +12,28 @@ import Result from '../Result';
 // == Import
 import './styles.css';
 
+//! On souhaite rajouter des eventLIstener etc :
+/* Objectif : pouvoir afficher ou masquer la liste des devises grâce à un bouton
+- en fonction d'une variable dans App, ne pas afficher Currencies
+- mettre en place un bouton
+- gérer un événement clic sur le bouton, et "connecter" le traitement avec la
+variable pour modifier l'affichage
+*/
+
 // == Composant APP :
-const App = () => (
-  <div className="app">
-    <Title />
-    <Currency
-      infos={currencies}
-    />
-    <Result
-      infos={currencies}
-    />
-  </div>
-);
+const App = () => {
+  const open = true;
+
+  return (
+    <div className="app">
+      <Title />
+      {open && <Currency infos={currencies} />}
+      <Result
+        infos={currencies}
+      />
+    </div>
+  );
+};
 
 // == Export
 export default App;
